0cce6b089b6b301a7856bebe7c11179a2d89c55d
MEDUZA <unkn.s0urc3@gmail.com>
2024-04-10 12:36:28 +0700

    Filling in the "main"

--- main.cpp
+++ main.cpp
@@ -47,5 +47,43 @@ void (*selectSortingFunction(int choice))(Book**, int) {
 }
 
 int main() {
+    const int size = 3;
+    Book* books[size];
+
+    books[0] = new Book{"Book1", "Author3", 2005};
+    books[1] = new Book{"Book2", "Author1", 1998};
+    books[2] = new Book{"Book3", "Author2", 2010};
+
+    std::cout << "Книги до сортировки:" << std::endl;
+    for (int i = 0; i < size; ++i) {
+        printBookInfo(books[i]);
+        std::cout << std::endl;
+    }
+
+    std::cout << "Выберите метод сортировки:" << std::endl;
+    std::cout << "1. По году издания" << std::endl;
+    std::cout << "2. По автору" << std::endl;
+
+    int choice;
+    std::cin >> choice;
+
+    void (*sortFunc)(Book**, int) = selectSortingFunction(choice);
+
+    if (sortFunc != nullptr) {
+        sortFunc(books, size);
+
+        std::cout << "Книги после сортировки:" << std::endl;
+        for (int i = 0; i < size; ++i) {
+            printBookInfo(books[i]);
+            std::cout << std::endl;
+        }
+    } else {
+        std::cout << "Неправильный выбор." << std::endl;
+    }
+
+    for (int i = 0; i < size; ++i) {
+        delete books[i];
+    }
+
     return 0;
 }
