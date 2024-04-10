e31be00fb7aa38ebf5d0eb7af3ff07fef64c0475
MEDUZA <unkn.s0urc3@gmail.com>
2024-04-10 12:26:32 +0700

    Added a function for displaying information about the book

--- main.cpp
+++ main.cpp
@@ -7,6 +7,12 @@ struct Book {
     int year;
 };
 
+void printBookInfo(Book* book) {
+    std::cout << "Title: " << book->title << std::endl;
+    std::cout << "Author: " << book->author << std::endl;
+    std::cout << "Year: " << book->year << std::endl;
+}
+
 int main() {
     return 0;
 }
