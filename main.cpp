2f57b7df1fdd3c234d09f6d7478c0e9d72b9e0b0
MEDUZA <unkn.s0urc3@gmail.com>
2024-04-10 12:27:52 +0700

    Added a function for sorting books by author

--- main.cpp
+++ main.cpp
@@ -25,6 +25,18 @@ void sortByYear(Book* books[], int size) {
     }
 }
 
+void sortByAuthor(Book* books[], int size) {
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = 0; j < size - i - 1; ++j) {
+            if (books[j]->author > books[j + 1]->author) {
+                Book* temp = books[j];
+                books[j] = books[j + 1];
+                books[j + 1] = temp;
+            }
+        }
+    }
+}
+
 int main() {
     return 0;
 }
